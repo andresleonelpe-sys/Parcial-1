46b2792c67f6a364af950120f67bc21c2853670a
andresleonelpe-sys <andresleonelpe@gmail.com>
2025-11-12 11:14:30 -0500

    Implement user notification strategies in App
    Refactored App class to implement notification strategies for users.

--- Punto2/src/App.java
+++ Punto2/src/App.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    import model.Notificacion;
+    import model.Usuario;
+    import service.GestorNotificaciones;
+    import strategy.*;
+    import java.util.Arrays;
+    import java.util.List;
+
+    public class App{
+        public static void app (String[] args){
+            Usuario u1 = new Usuario("Ana", "ana@example.com", new EstrategiaEmail());
+            Usuario u2 = new Usuario("Luis", "555-1234", new EstrategiaSMS());
+            Usuario u3 = new Usuario("Marta", "marta@example.com", new EstrategiaPush());
+            List<Usuario> usuarios = Arrays.asList(u1, u2, u3);
+
+            GestorNotificaciones gestor = new GestorNotificaciones();
+            Notificacion notificacion = new Notificacion("¡Tienes una nueva notificacion!", null);
+            gestor.enviarNotificaciones(usuarios, notificacion);
+
+            u1.setEstrategia(new EstrategiaPush());
+            Notificacion notificacion2 = new Notificacion("¡Tu estrategia de notificación ha cambiado!", null);
+            gestor.enviarNotificaciones(Arrays.asList(u1), notificacion2);
+
+        };
     }
-}
+
